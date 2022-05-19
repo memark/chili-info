7b628d790775d85054c3f9660a92e16c309b6093
Magnus Markling <magnus@markling.com>
2022-05-19 20:23:32 +0200

    add search box

---
+++ src/components/ChiliInfo.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const name = ref('')
+</script>
+
+<template>
+  <h1>Chili Info</h1>
+
+  <input v-model="name" placeholder="Search for chili" />
+
+  <p>{{ name }}</p>
+</template>
